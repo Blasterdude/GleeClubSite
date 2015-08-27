9afb5656bc395aa8c8cc12ffae1cf2a786511aa7
Thomas Meehan <tommeehan8@gmail.com>
2015-08-27 00:43:37 -0400

    First (silly) commit

---
+++ js/colap.js
@@ -0,0 +1,7 @@
+$('.row .but').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+});
+
